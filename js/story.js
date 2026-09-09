903c3c2ef8681ed59c35507ca7d159d501a404a1
貓頭鷹 <112803667+chidori2164@users.noreply.github.com>
2026-09-09 21:38:19 +0800

    Create story.js

---
+++ js/story.js
@@ -0,0 +1,61 @@
+// ==========================
+// GAL 劇情資料
+// ==========================
+
+const story = [
+
+    {
+        background: "學校・教室",
+
+        character: "●",
+
+        name: "秋山",
+
+        text: "早安。"
+    },
+
+
+    {
+        background: "學校・教室",
+
+        character: "●",
+
+        name: "秋山",
+
+        text: "今天也來得很早呢。"
+    },
+
+
+    {
+        background: "學校・教室",
+
+        character: "●",
+
+        name: "我",
+
+        text: "因為今天不小心早醒了。"
+    },
+
+
+    {
+        background: "學校・教室",
+
+        character: "●",
+
+        name: "秋山",
+
+        text: "這樣啊。"
+    },
+
+
+    {
+        background: "學校・教室",
+
+        character: "●",
+
+        name: "秋山",
+
+        text: "那我們一起去上課吧。"
+    }
+
+];
